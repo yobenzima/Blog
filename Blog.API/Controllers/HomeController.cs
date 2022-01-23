1597f41d50debf141e6db57152a7f9b5501ac25d
Yobe <yobenzima@yahoo.com>
2022-01-23 15:26:20 +0200

    Initial Commit

---
+++ Blog.API/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.API.Controllers
+{
+    [ApiController]
+    [Route("api/home")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> mLogger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            mLogger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return Ok(new { Name = "Test"} );
+        }
+    }
+}
---
+++ Blog.API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.API.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+    }
+}
